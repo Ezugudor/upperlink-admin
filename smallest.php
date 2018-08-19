59584552e3c56e40ef38e8d0e335fe5caf780988
Ezugudor <nelsonsmrt@gmail.com>
2018-08-19 20:23:01 +0100

    also taking care of the negative values

--- smallest.php
+++ smallest.php
@@ -4,12 +4,20 @@ function getSmallest($Arr){
 	$min = min($Arr);
 	$missing = array();
 	$smallest = $min;
+	
+	// if no poisitive value in the input array , stop execution and return 1
+	if($max < 0){
+		return 1;
+	}
+ 
+	// go through the array and get values not in the array. Stack them into another array MISSING
 	for ($i=1; $i < $max ; $i++) { 
 		if(!in_array($i, $Arr)){
 			     array_push($missing, $i);
 		}
 	}
-
+    
+    //if there is no missing value between the smallest and the highest value , then the next smallest value is definitely the next to the highest i.e HIGHEST + 1
 	if(count($missing) == 0){
 		$smallest = $max+1;
 	}else{
@@ -19,7 +27,7 @@ function getSmallest($Arr){
 	return $smallest;
 }
 
-
-echo getSmallest([-1,0,2,4,2,9]);
+//TEST
+echo getSmallest([-1,-34,-4,1,2,3,44,5,5,555,6,6,67,7,7,8,8,9]);
 
 ?>
